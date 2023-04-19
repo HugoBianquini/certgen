bb665cd524af7f8b19e3f4368f5bd4f16b340e37
HugoBianquini <hugobianqui@hotmail.com>
2023-04-19 20:54:56 -0300

    create certificate and compile it using handlebars

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,10 +1,31 @@
 import { APIGatewayProxyHandler } from "aws-lambda"
-import { document } from "../../utils/dynamodbClient"
+import { compile } from "handlebars"
+import { document } from "../utils/dynamodbClient"
+import { join } from "path";
+import { readFileSync } from "fs";
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
+}
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
+
+const compileTemplate = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), "src", "templates", "certificate.hbs")
+
+  const html = readFileSync(filePath, "utf-8");
+
+  const compiledHtml = compile(html);
+
+  return compiledHtml(data);
 }
 
 export const handler: APIGatewayProxyHandler = async (event) => {
@@ -29,6 +50,19 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       ":id": id
     }
   }).promise()
+
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+  const medal = readFileSync(medalPath, "base64");
+
+  const data: ITemplate = {
+    name,
+    id,
+    grade,
+    date: new Date().toLocaleDateString("pt-BR"),
+    medal
+  }
+
+  const content = await compileTemplate(data)
 
   return {
     statusCode: 201,
